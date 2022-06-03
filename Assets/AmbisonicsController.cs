a0bf198746208a720a57d3ea65ba26731614ce71
unknown <me@neolyum.de>
2022-06-03 09:45:27 +0200

    joa

--- Assets/AmbisonicsController.cs
+++ Assets/AmbisonicsController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class AmbisonicsController : MonoBehaviour
 {
+    private SteamAudio.SteamAudioAmbisonicSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<SteamAudio.SteamAudioAmbisonicSource>();
     }
 
     // Update is called once per frame
